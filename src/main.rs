c5875f1630b3bfddc761ebbd76c035de73a8c2dc
Reykdal <enok24@ru.is>
2026-09-23 14:06:10 +0000

    Guardian IPv6 package code, guardian.rs þ.e.a.s.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 mod evil;
 mod net;
 mod secret;
+mod guardian;
 
 use net::{make_socket, send_and_recv};
 use std::net::{Ipv4Addr, UdpSocket};
@@ -78,9 +79,9 @@ fn main() -> Result<(), String> {
             } else if text.contains("https://en.wikipedia.org/wiki/Evil_bit") {
                 println!("port {port} => EVIL!");
                 ports.evil = port;
-            } else if text.contains("guardian of the secret spell") {
-                println!("port {port} => IPv6");
-                ports.ipv6 = port;
+              else if text.contains("guardian of the secret spell") {
+                println!("port {port} => GUARDIAN");
+                guardian_port = Some(port);
             }
         }
     }
@@ -96,6 +97,10 @@ fn main() -> Result<(), String> {
         "\nGOT port={} phrase={}",
         evil_port.hidden_port, evil_port.phrase
     );
+
+    let gp = guardian_port.expect("could not find the Guardian port");
+    let guard = guardian::solve(&sock, cfg.ip, gp, &res)?;
+    println!("GUARDIAN reveal: {}", guard.reveal);
 
     Ok(())
 }
